a10dfb56a5516b3251d62782fb0088ad1480a279
volucrisscr <quinn@volucris.space>
2018-02-20 20:33:30 -0500

    Fix rendering bug in Firefox

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,14 +1,6 @@
 import Typography from "typography"
 import stAnnesTheme from "typography-theme-st-annes"
 
-stAnnesTheme.googleFonts = [
-  {
-    name: 'Source Serif Pro',
-    styles: [
-      '600',
-    ],
-  },
-]
 
 const typography = new Typography(stAnnesTheme)
 
