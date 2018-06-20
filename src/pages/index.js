a8a15884cd2a4c0d7c0b2f9c834caa4b1eec340d
Quinn James <quinn@quinnjames.net>
2018-06-20 12:06:12 -0400

    Fixed PDF and project links; updated website generally

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,57 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import resume from './Quinn.pdf'
+console.log(resume);
 
 const IndexPage = () => (
+
   <div style={{ margin: '.8rem auto', maxWidth: 620 }}>
     <div>
       <h1>
         About me
       </h1>
       <p>
-        I'm a front-end web developer, with my greatest focus on JS/Angular/Ionic. I've always had a passion for knowledge of all
-        kinds. Now, I'm excited for the potential for web applications to manage complexity and transmit
-        useful knowledge to users.
+      I am a front­end focused web developer/engineer. I am primarily developing in modern JavaScript (ES6), 
+      using Vue and Angular. I have experience with .NET, Ionic and Flask. I want to build cool stuff!
       </p>
       <p>
 
-        Outside of coding, I'm a <a href="http://www.cross-tables.com/results.php?p=1822">tournament Scrabble player</a>,
-        and I wrote <a href="resource/the-devil-in-the-shape-of-a-german-causes-and-conceptions.html">an award-winning essay</a> about European
-        witch trials.
+        Outside of coding, I'm a <a href="http://www.cross-tables.com/results.php?p=1822" target="_blank">tournament Scrabble player</a>.
       </p>
       <p>
-        If you're looking for my resume, it's <a href="http://www.volucris.space/resource/QuinnJamesResume.pdf">here (PDF)</a>.
+        If you're looking for my resume, it's <a href={resume}>here (PDF)</a>.
       </p>
 
     </div>
 
-    <div>
-      <h1>Technologies</h1>
-      <p>
-        I'm most experienced with modern <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</a>
-      , <a href="https://www.typescriptlang.org/">TypeScript</a>,
-      and <a href="http://angular.io/">Angular</a>.
-      I've built an app, <a href="http://mondain.herokuapp.com">Mondain Words</a>, using Typescript, Angular, and <a href="https://ionicframework.com/">Ionic</a>                            with a <a href="https://firebase.google.com/">Firebase</a> backend. The app also uses <a href="https://github.com/angular/angularfire2">AngularFire</a>                            for authentication.
-    </p>
-    <p>
-      I also have some experience with non-JS languages. For data and string manipulation, I
-      like <a href="https://www.python.org/">Python</a>&mdash;I used it to coerce SQLite data into JSON and then rearrange that data for Mondain.
-      I also spent a good deal of time with C#/.NET.
-    </p>
-
-  </div>
   <div>
-    <h1>Apps</h1>
+    <h1>Projects</h1>
 
     <ul>
-      <li>
+    <li>
         <p>
-          <a href="http://mondainwords.herokuapp.com" target="_blank">
+          <a href="http://featurerequester.herokuapp.com" target="_blank">
             <b>
-              Mondain Words
+              Feature Requester
             </b>
           </a>
-          : A web app to practice unscrambling words. Ionic/Angular/Typescript/Firebase.
-          Success in Scrabble requires the accumulation of a vast amount of information, and some
-          way to manage that complexity. The goal of Mondain is to improve the state of the art
-          in those areas. Source code <a href="https://github.com/quinnajames/mondain-ionic">here</a>.
+          : This is a fairly simple project using Knockout.js and Flask. It allows you to create, update and delete
+          product feature requests and display them in an attractive layout. No login is required.
+          (<a href="https://github.com/quinnajames/mondain-ionic" target="_blank">GitHub source</a>)
         </p>
       </li>
       <li>
         <p>
-          <a href="http://colortheworld.herokuapp.com" target="_blank">
+          <a href="http://mondain.herokuapp.com" target="_blank">
             <b>
-              Color the World
+              Mondain Words
             </b>
           </a>
-          : A color-picker and saver. Select a color, and get shown an array of complementary colors.
-          If you like the set, click to save it. Then tab over to the gallery to see what you've
-          previously saved.
+          : A web app to practice unscrambling words. Ionic, Angular, Typescript, Firebase.
+          Success in Scrabble requires the accumulation of a vast amount of information, and some
+          way to manage that complexity. The goal of Mondain is to improve the state of the art
+          in those areas. You do need to login with an email address to try it.
+          (<a href="https://github.com/quinnajames/feature_requester" target="_blank">GitHub source</a>)
         </p>
       </li>
     </ul>
@@ -74,19 +60,9 @@ const IndexPage = () => (
   <div>
     <h1>Other Projects</h1>
     <ul>
-      <li>
-        <p>
-          <a href="http://www.volucris.space/kanji-quiz/kanji.html">
-            Kanji Quiz
-          </a>
-          : Practice recognizing Japanese kanji characters. Customize whether
-          to respond with English or Japanese <i>on</i> or <i>kun</i> readings, and the font size.
-          JS/JQuery. Source code <a href="https://github.com/quinnajames/kanji-quiz">here.</a>
-      </p>
-    </li>
     <li>
       <p>
-        <b>Lexcheck</b> (<a href="https://github.com/quinnajames/lexcheck">GitHub source</a>):
+        <b>Lexcheck</b> (<a href="https://github.com/quinnajames/lexcheck" target="_blank">GitHub source</a>):
           A small Python utility to check typographical errors in lists against
           a lexicon you provide. Useful for word game players preparing study lists, for instance.
         </p>
@@ -96,7 +72,7 @@ const IndexPage = () => (
         <p>
           <b>
             Ancient Blocks
-          </b> (<a href="https://github.com/quinnajames/ancient_blocks">GitHub source</a>):
+          </b> (<a href="https://github.com/quinnajames/ancient_blocks" target="_blank">GitHub source</a>):
           A minigame made in C++ with the Allegro game library.
         </p>
       </li>
@@ -110,37 +86,37 @@ const IndexPage = () => (
     <ul>
       <li>
         <p>
-          Twitter: <a href="https://twitter.com/quinnajames">quinnajames</a>
+          Email: <a href="mailto:quinn@quinnjames.net">quinn@quinnjames.net</a>
+      </p>
+      </li>
+      <li>
+        <p>
+          Phone: (843) 209<span>-6331</span>
+        </p>
+      </li>
+      <li>
+        <p>
+          Twitter: <a href="https://twitter.com/quinnajames" target="_blank">quinnajames</a>
       </p>
     </li>
     <li>
       <p>
-        Github: <a href="https://github.com/quinnajames/">quinnajames</a>
+        GitHub: <a href="https://github.com/quinnajames/" target="_blank">quinnajames</a>
     </p>
   </li>
 
   <li>
     <p>
-      LinkedIn: <a href="https://www.linkedin.com/in/quinnjamestech/">
+      LinkedIn: <a href="https://www.linkedin.com/in/quinnjamestech/" target="_blank">
       Quinn James
     </a>
-  </p>
-</li>
-<li>
-  <p>
-    Email: <a href="mailto:quinn@quinnjames.net">quinn@quinnjames.net</a>
-</p>
-</li>
-<li>
-  <p>
-    Phone: (843) 209<span>-6331</span>
   </p>
 </li>
 </ul>
 
 </div>
 <div>
-  This site was built with <a href="https://www.gatsbyjs.org/">Gatsby.js</a> and <a href="https://kyleamathews.github.io/typography.js/">Typography.</a>
+  This site was built with the help of <a href="https://www.reactjs.org/" target="_blank">React</a>, <a href="https://www.gatsbyjs.org/" target="_blank">Gatsby</a> and <a href="https://kyleamathews.github.io/typography.js/" target="_blank">Typography.</a>
 
 </div>
 </div>
